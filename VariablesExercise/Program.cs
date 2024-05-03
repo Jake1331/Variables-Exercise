4362980d3c910cda09119229ab00b7273aa2e407
Jake1331 <michaeldruummer@gmail.com>
2024-05-02 23:38:40 -0500

    Finisehd the exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -2,9 +2,25 @@
 {
     public class Program
     {
+        
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        string name = "Michael";
+        int age = 26;
+        char middleInitial = 'J';
+        bool isOver18 = true;
+        double currentTemp = 72.5;
+        decimal currentHeight = 6.0m;
+
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Middle Initial: {middleInitial}");
+            Console.WriteLine($"Is over 18: {isOver18}");
+            Console.WriteLine($"Current Temp: {currentTemp}");
+            Console.WriteLine($"Current Height: {currentHeight}ft");
         }
     }
 }
